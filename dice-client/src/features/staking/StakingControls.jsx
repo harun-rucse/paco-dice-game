b3418689d0305e22193e0f23cfee07017dd728fc
Md. Harun Or Rashid <horrashid007@gmail.com>
2023-12-23 13:02:04 +0600

    About staking modal added

--- dice-client/src/features/staking/StakingControls.jsx
+++ dice-client/src/features/staking/StakingControls.jsx
@@ -1,14 +1,24 @@
 import Modal from "../../components/Modal";
+import AboutStaking from "./AboutStaking";
 import StakingCalculator from "./StakingCalculator";
 
 function StakingControls() {
   return (
     <div className="flex items-center gap-3 lg:gap-8">
-      <button className="button text-sm lg:text-lg">About Staking</button>
+      <Modal>
+        <Modal.Open opens="about-staking">
+          <button className="button text-sm lg:text-lg">About Staking</button>
+        </Modal.Open>
+        <Modal.Body name="about-staking" className="calculator-gradient-bg">
+          <AboutStaking />
+        </Modal.Body>
+      </Modal>
+
       <button className="button text-sm lg:text-lg flex gap-1 items-center">
         <img src="/icons/paco-icon.png" alt="" className="w-5 lg:w-7" />
         <span>About Paco</span>
       </button>
+
       <Modal>
         <Modal.Open opens="staking-calculator">
           <button className="button text-sm lg:text-lg !bg-[#32a45f] !border-[#316A48]">
