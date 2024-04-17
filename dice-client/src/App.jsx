152e4e6933f9cb47fa6c593e26629f5c5be1341f
Md. Harun Or Rashid <horrashid007@gmail.com>
2024-04-17 13:23:22 +0600

    Refactor router logic to seperate routers

--- dice-client/src/App.jsx
+++ dice-client/src/App.jsx
@@ -1,29 +1,12 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import PageNotFound from "./pages/PageNotFound";
+import Routes from "./routes";
 import Spinner from "./components/Spinner";
 import { BalanceProvider } from "./context/BalanceContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-import PublicdRoute from "./components/PublicRoute";
-import StakingLayout from "./components/StakingLayout";
-
-const AppLayout = lazy(() => import("./components/AppLayout"));
-const AdminLayout = lazy(() => import("./components/admin/AdminLayout"));
-const ComingSoon = lazy(() => import("./pages/ComingSoon"));
-const Homepage = lazy(() => import("./pages/Homepage"));
-const AdminDashboard = lazy(() => import("./pages/AdminDashboard"));
-const AdminApproval = lazy(() => import("./pages/AdminApproval"));
-const ResetPassword = lazy(() => import("./pages/ResetPassword"));
-const Profile = lazy(() => import("./pages/Profile"));
-const AdminWithdrawable = lazy(() => import("./pages/AdminWithdrawable"));
-const AdminDeposit = lazy(() => import("./pages/AdminDeposit"));
-const Staking = lazy(() => import("./pages/Staking"));
-const AdminBurn = lazy(() => import("./pages/AdminBurn"));
-const Lottery = lazy(() => import("./pages/Lottery"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,52 +25,7 @@ function App() {
 
         <BrowserRouter>
           <Suspense fallback={<Spinner />}>
-            <Routes>
-              <Route path="/" element={<AppLayout />}>
-                <Route index element={<Homepage />} />
-
-                <Route
-                  path="/profile"
-                  element={
-                    <ProtectedRoute roles={["admin", "user"]}>
-                      <Profile />
-                    </ProtectedRoute>
-                  }
-                />
-
-                <Route
-                  path="/reset-password/:resetToken"
-                  element={
-                    <PublicdRoute>
-                      <ResetPassword />
-                    </PublicdRoute>
-                  }
-                />
-
-                <Route path="*" element={<PageNotFound />} />
-              </Route>
-              <Route path="/staking" element={<StakingLayout />}>
-                <Route index element={<Staking />} />
-              </Route>
-              <Route path="/lottery" element={<StakingLayout />}>
-                <Route index element={<Lottery />} />
-              </Route>
-              <Route
-                path="/admin"
-                element={
-                  <ProtectedRoute roles={["admin"]}>
-                    <AdminLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route index element={<Navigate replace to="dashboard" />} />
-                <Route path="dashboard" element={<AdminDashboard />} />
-                <Route path="approval" element={<AdminApproval />} />
-                <Route path="withdrawables" element={<AdminWithdrawable />} />
-                <Route path="add-deposit" element={<AdminDeposit />} />
-                <Route path="burn" element={<AdminBurn />} />
-              </Route>
-            </Routes>
+            <Routes />
           </Suspense>
         </BrowserRouter>
         <Toaster
--- dice-client/src/pages/PageNotFound.jsx
+++ dice-client/src/pages/PageNotFound.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 function PageNotFound() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-[#2e2550] text-white px-7 py-7 shadow-2xl rounded-3xl">
+    <div className="w-full h-screen flex justify-center items-center bg-[#2e2550] text-white px-7 py-7 shadow-2xl">
       <div className="flex flex-col text-center">
         <h1 className="text-4xl uppercase font-extralight mt-4">
           Page Not Found!
         </h1>
         <p className="text-gray-200 mt-2">This page doesn't exists!</p>
+        <Link
+          to="/"
+          className="self-center bg-[#67a455] text-white px-4 py-2 rounded-md mt-6"
+        >
+          Back to Home
+        </Link>
       </div>
     </div>
   );
